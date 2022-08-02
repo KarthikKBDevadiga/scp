b673e54db13aed46cde75e42349e94cfb1918553
karthikkbdl4o <109502155+karthikkbdl4o@users.noreply.github.com>
2022-08-02 18:59:43 +0530

    02 08 2022

--- src/routes/login/Login.js
+++ src/routes/login/Login.js
@@ -1,3 +1,4 @@
+import ArrowIcon from "../../icons/ArrowIcon";
 import LockIcon from "../../icons/LockIcon";
 import MailIcon from "../../icons/MailIcon";
 import "./Login.css";
@@ -5,7 +6,7 @@ const Login = () => {
   return (
     <div className="main">
       <div className="card">
-        <div className="heading">Login</div>
+        <div className="heading">Member Login</div>
         <div
           style={{
             marginTop: "48px",
@@ -24,19 +25,10 @@ const Login = () => {
           <LockIcon className="inputFieldIcon" />
           <input placeholder="password" type="password" />
         </div>
+
         <div
           style={{
-            marginTop: "16px",
-            textAlign: "right",
-            color: "#1d4ed8",
-            fontSize: "14px",
-          }}
-        >
-          Forget Password?
-        </div>
-        <div
-          style={{
-            marginTop: "32px",
+            marginTop: "64px",
           }}
           className="loginButton"
         >
@@ -44,12 +36,37 @@ const Login = () => {
         </div>
         <div
           style={{
+            textAlign: "center",
+            marginTop: "16px",
+          }}
+        >
+          Forgot <span style={{ fontWeight: "bold" }}>Username</span> /{" "}
+          <span style={{ fontWeight: "bold" }}>Password</span>?
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "64px",
+            textAlign: "center",
+            gap: "8px",
+            cursor: "pointer",
+          }}
+        >
+          <div style={{ alignSelf: "center", fontWeight: "bold" }}>
+            Create your account
+          </div>
+          <ArrowIcon style={{ alignSelf: "center" }} />
+        </div>
+        {/* <div
+          style={{
             marginTop: "16px",
           }}
           className="signupButton"
         >
           SIGN UP
-        </div>
+        </div> */}
       </div>
     </div>
   );
